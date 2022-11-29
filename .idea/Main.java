d86ef72366000909cc790366559f604c080a1a96
natalielearnscode <43387140+natalielearnscode@users.noreply.github.com>
2022-11-29 12:24:32 -0500

    updating class

---
+++ .idea/Main.java
@@ -0,0 +1,16 @@
+public class UpdateUser {
+
+    public static void main(String[] args) {
+
+//Update the user and any of their fields except for email
+        User user = new User();
+
+        user.setFirstName("John");
+        user.setLastName("Smith");
+        user.setUsername("jsmith");
+        user.setPassword("123456");
+
+//Update the user in the database
+        UserDAO.updateUser(user);
+    }
+}
