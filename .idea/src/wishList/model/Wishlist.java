eac47f2d6c073bcc44971edc669f707e8852c647
camcast04 <ccast049@fiu.edu>
2022-12-01 17:38:59 -0500

    Here ya go 2

--- .idea/src/wishList/model/Wishlist.java
+++ .idea/src/wishList/model/Wishlist.java
@@ -5,6 +5,21 @@ public class Wishlist {
     private String name;
     private LocalDate dateCreated;
     private String book;
+    private String user;
+
+
+    public Wishlist(String name,
+                    LocalDate dateCreated,
+                    String book,
+                    String user) {
+        this.name = name;
+        this.dateCreated = dateCreated;
+        this.book = book;
+        this.user = user;
+    }
+
+    public Wishlist() {
+    }
 
     public String getName() {
         return name;
@@ -30,6 +45,36 @@ public class Wishlist {
         this.book = book;
     }
 
+    public String getUser() {
+        return user;
+    }
 
+    public void setUser(String user) {
+        this.user = user;
+    }
 
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Wishlist wishlist = (Wishlist) object;
+        return java.util.Objects.equals(name, wishlist.name)
+                && java.util.Objects.equals(dateCreated, wishlist.dateCreated)
+                && java.util.Objects.equals(book, wishlist.book)
+                && java.util.Objects.equals(user, wishlist.user);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(super.hashCode(), name, dateCreated, book, user);
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Wishlist{" +
+                "name='" + name + '\'' +
+                ", dateCreated=" + dateCreated +
+                ", book='" + book + '\'' +
+                ", user='" + user + '\'' +
+                '}';
+    }
 }
